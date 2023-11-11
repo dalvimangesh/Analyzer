5e4545fd09772985e8e9794db682bce2d4c044fc
Thanmayee Gunja <114472443+thanmayee07@users.noreply.github.com>
2023-11-11 11:23:26 +0530

    Updated

--- Analyzer/Pipeline/AvoidSwitchStatements.cs
+++ Analyzer/Pipeline/AvoidSwitchStatements.cs
@@ -1,35 +1,30 @@
 using Analyzer.Parsing;
 using Mono.Cecil.Cil;
 using Mono.Cecil;
+using System;
 
 namespace Analyzer.Pipeline
 {
     /// <summary>
-    /// Analyzer rule to detect the presence of switch statements in methods.
+    /// Analyzer rule for detecting switch statements in methods.
     /// </summary>
     public class AvoidSwitchStatementsAnalyzer : AnalyzerBase
     {
-        // Fields to store analysis results
         private string errorMessage;
         private int verdict;
         private readonly string analyzerID;
 
-        /// <summary>
-        /// Initializes a new instance of the AvoidSwitchStatementsAnalyzer class.
-        /// </summary>
-        /// <param name="dllFiles">The ParsedDLLFiles object containing the parsed DLL information.</param>
         public AvoidSwitchStatementsAnalyzer(ParsedDLLFiles dllFiles) : base(dllFiles)
         {
-            // Initialize fields
             errorMessage = "";
             verdict = 1;
-            analyzerID = "avoidSwitchStatements";
+            analyzerID = "104";
         }
 
         /// <summary>
-        /// Runs the analysis to check for switch statements in methods.
+        /// Runs the analysis to check for the presence of switch statements in methods.
         /// </summary>
-        /// <returns>An AnalyzerResult object indicating the analysis result.</returns>
+        /// <returns>An <see cref="AnalyzerResult"/> based on the analysis.</returns>
         public override AnalyzerResult Run()
         {
             CheckForSwitchStatements();
@@ -41,22 +36,19 @@ namespace Analyzer.Pipeline
         /// </summary>
         private void CheckForSwitchStatements()
         {
-            // Iterate through each class and its methods
             foreach (ParsedClassMonoCecil cls in parsedDLLFiles.classObjListMC)
             {
                 foreach (MethodDefinition method in cls.MethodsList)
                 {
-                    // Check if the method has a body
                     if (method.HasBody)
                     {
-                        // Iterate through each instruction in the method's body
                         foreach (Instruction instruction in method.Body.Instructions)
                         {
-                            // Check if the instruction is a switch statement
                             if (instruction.OpCode == OpCodes.Switch)
                             {
+                                // Modify the verdict and errorMessage as needed for reporting.
                                 verdict = 0;
-                                errorMessage = "Switch statement found.";
+                                errorMessage = $"Switch statement found in method {method.FullName}.";
                                 return; // You can return early if a switch statement is found.
                             }
                         }
